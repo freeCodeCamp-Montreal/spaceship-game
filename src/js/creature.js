114a5dc9cecc9b83223ae71e3cbea55f2bc057de
Tiffany Le-Nguyen <tlenguyen@expedia.com>
2019-01-18 16:31:14 -0500

    🔨 Simplify logic and update spaceship.svg

--- src/js/creature.js
+++ src/js/creature.js
@@ -32,25 +32,36 @@ module.exports = class Creature {
     this.move();
   }
 
+  /**
+   * Handles the movement for a creature as well as what happens
+   * when it moves to a spot where there is a ship.
+   */
   move() {
     this.baddyStepInterval = setInterval(() => {
+      // If the creature died, remove from the DOM and clear our interval
+      if (Array.from(this.creature.classList).includes('baddie-died')) {
+        this.creature.remove();
+        window.clearInterval(this.baddyStepInterval);
+      }
+
       const x = this.left();
       const y = this.top();
 
+      // Hit box (y axis) where the creature would hit our ship
       const hitBoxMax = this.ship.height + this.ship.top();
       const hitBoxMin = this.ship.top() - this.ship.height;
 
       // 80 represents the x position of our ship
       if (x <= 80 && y < hitBoxMax && y > hitBoxMin) {
-        // If the creature died, remove from the DOM and clear our interval
-        if (Array.from(this.creature.classList).includes('baddie-died')) {
-          this.creature.remove();
+        // this, if it worked, would only stop the current interval, all other badies will continue moving
+        if (this.ship.lives === 0) {
+          g.gameOver(this.baddyStepInterval)
         } else {
-          // this, if it worked, would only stop the current interval, all other badies will continue moving
-          if (this.ship.lives === 0) g.gameOver(this.baddyStepInterval);
-          else {this.ship.loseLife(); console.log(this.ship.lives) }
+          this.ship.loseLife();
+          this.creature.src = "static/boom.svg"
+          this.creature.classList.add("baddie-died");
+          console.log(this.ship.lives)
         }
-        window.clearInterval(this.baddyStepInterval);
       }
 
       this.creature.style.left = `${x - ((this.step + 1) * 2)}px`;
@@ -79,8 +90,8 @@ module.exports = class Creature {
 
   isShipHit(ship) {
     return this.left() < ship.right() &&
-        this.right() > ship.left() &&
-        this.top() < ship.bottom() &&
-        this.bottom() > ship.top();
+      this.right() > ship.left() &&
+      this.top() < ship.bottom() &&
+      this.bottom() > ship.top();
   }
 };
